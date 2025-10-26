87543544ae2809e0922d42ebfb30123822dce22e
Michal Rakowski <mike@lucinity.com>
2025-10-26 12:46:59 +0100

    tailwind - INTRODUCED THE BUG!

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,8 @@ import { RadioGroup, Radio, Label } from "react-aria-components";
 function App() {
   const [selectedDrone, setSelectedDrone] = useState("huey");
   return (
-    <div
-      style={{
-        backgroundColor: "rgb(239, 68, 68)",
-        display: "grid",
-        gridTemplateColumns: "1fr 4fr",
-        gridTemplateRows: "repeat(1, minmax(0, 1fr))",
-        minHeight: "100vh",
-        padding: 0,
-        margin: 0,
-        height: "100vh",
-      }}
-    >
-      <div
-        style={{
-          backgroundColor: "rgb(59, 130, 246)",
-          overflow: "scroll",
-        }}
-      >
+    <div className="bg-red-500 grid grid-cols-[1fr_4fr] grid-rows-1 min-h-screen p-0 m-0 h-screen">
+      <div className="bg-blue-500 overflow-scroll">
         Cascading Style Sheets (CSS) is a style sheet language used for
         specifying the presentation and styling of a document written in a
         markup language such as HTML or XML (including XML dialects such as SVG,
@@ -41,12 +25,7 @@ function App() {
         XML dialects such as SVG, MathML or XHTML).[2] CSS is a cornerstone
         technology of the World Wide Web, alongside HTML and JavaScript.[3]
       </div>
-      <div
-        style={{
-          backgroundColor: "rgb(34, 197, 94)",
-          overflow: "scroll",
-        }}
-      >
+      <div className="bg-green-500 overflow-scroll">
         Hypertext Markup Language (HTML) is the standard markup language[a] for
         documents designed to be displayed in a web browser. It defines the
         content and structure of web content. It is often assisted by
